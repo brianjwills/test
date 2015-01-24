b4d82792d931e391cceb69e39541335e800228a0
brianjwills <brian.wills@gmail.com>
2015-01-23 21:02:30 -0800

    Create rsync_uploads.pl

---
+++ rsync_uploads.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use DBI;
+
+my $dsn = 'aisleiq_demo_production';
+my $user = '';
+my $password = '';
+
+$dbh = DBI->connect($dsn, $user, $password,
+                    { RaiseError => 1, AutoCommit => 0 });
+$sth = $dbh->prepare("SELECT `pos_upload_directory` FROM `store` WHERE `active_pos_uploader` = 1");
+$sth->execute();
+while ( my @row = $sth->fetchrow_array ) {
+  `rsync -va $row[0] aiq@saiq:$row[0]`;
+}
